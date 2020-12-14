099711ad82a849e061de63b4d0f6da8b334d92bb
milad-hamedi <m_h@mailfa.com>
2020-12-14 05:21:51 -0800

    8.9

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ const memory = navigator.deviceMemory
 document.getElementById("ram").innerHTML = `This device has at least  ${memory} GiB of RAM.`;
 //------------------------------
 //online or offline
- if(navigator.onLine === true)
+/*  if(navigator.onLine === true)
  {
  document.getElementById("net").innerHTML = `You are online`;
    }
      else
    { document.getElementById("net").innerHTML = `offline`;}
-	 
+	  */
 //-------------------------------
 //cpu core
 // gets the "available" logical processors count
-var logicalProcessorCount = navigator.hardwareConcurrency;
-document.getElementById("cpu_core").innerHTML = `your Cpu_core:${logicalProcessorCount}Core`;
+//var logicalProcessorCount = navigator.hardwareConcurrency;
+//document.getElementById("cpu_core").innerHTML = `your Cpu_core:${logicalProcessorCount}Core`;
