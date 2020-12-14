170cb67ce0944d37910b51f8aec54cef7218aac3
milad-hamedi <m_h@mailfa.com>
2020-12-14 00:28:52 -0800

    test6

--- app.js
+++ app.js
@@ -4,14 +4,37 @@ var ram = navigator.deviceMemory
 
 //------------------------------
 //online or offline
-window.addEventListener('online', function() {
-	alert('Current status : online');
-});
+// window.addEventListener('online', function() {
+// 	alert('Current status : online');
+// });
 
-window.addEventListener('offline', function() {
-	alert('Current status : offline');
-});
+// window.addEventListener('offline', function() {
+// 	alert('Current status : offline');
+// });
 
+// // self executing function here
+// (function() {
+//     // your page initialization code here
+//     // the DOM will be available here
+//     alert('Current status : online');
+ 
+//  })();
+window.addEventListener('load', function() {
+    var status = document.getElementById("status");
+    var log = document.getElementById("log");
+  
+    function updateOnlineStatus(event) {
+      var condition = navigator.onLine ? "online" : "offline";
+  
+      status.className = condition;
+      status.innerHTML = condition.toUpperCase();
+  
+      log.insertAdjacentHTML("beforeend", "Event: " + event.type + "; Status: " + condition);
+    }
+  
+    window.addEventListener('online',  updateOnlineStatus);
+    window.addEventListener('offline', updateOnlineStatus);
+  });
 //-------------------------------
 //cpu core
 // gets the "available" logical processors count
