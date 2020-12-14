1a6ff812023e72dd353993e5b623dec1276cad7b
milad-hamedi <m_h@mailfa.com>
2020-12-14 00:15:24 -0800

    test5.1

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var ram = navigator.deviceMemory
 
-
+//------------------------------
+//online or offline
 window.addEventListener('online', function() {
 	alert('Current status : online');
 });
@@ -8,3 +9,8 @@ window.addEventListener('online', function() {
 window.addEventListener('offline', function() {
 	alert('Current status : offline');
 });
+
+//-------------------------------
+//cpu core
+// gets the "available" logical processors count
+var logicalProcessorCount = navigator.hardwareConcurrency;
